b8a21253c56a73314118201ecddb61bd51dd534a
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-01-21 21:19:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,7 +41,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('DONT ADD ME TO GROUPS', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('DONT ADD ME TO GROUPS', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
             InlineKeyboardButton('Mᴏᴠɪᴇs Aʟʟ', url='')
             ],[
             InlineKeyboardButton('Sᴇᴀʀᴄʜ', switch_inline_query_current_chat='https://t.me/tomoviesall'),
