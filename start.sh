e663f86744d025ff85838feda645142b6236847f
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-01-22 17:02:59 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/TELEZEN/ercel
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /ercel
+fi
+cd /ercel
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
